05c41554739416b9553eb8615762f998453f4ed0
Judgebread <judgebread@yandex.ru>
2022-01-14 23:05:28 +0300

    base

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Base msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Base from './components/Base.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Base
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
